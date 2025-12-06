18f47d38b326a591f694636a440e0e383f3c6efd
tatdat <tatdat1412@gmail.com>
2025-12-07 01:02:22 +0700

    delete some exception

--- service/src/main/java/com/example/service/exception/UnauthorizedException.java
+++ service/src/main/java/com/example/service/exception/UnauthorizedException.java
@@ -1,8 +1,8 @@
-package com.example.service.exception;
-
-// 401 - chưa đăng nhập hoặc token sai
-public class UnauthorizedException extends RuntimeException{
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
+//package com.example.service.exception;
+//
+//// 401 - chưa đăng nhập hoặc token sai
+//public class UnauthorizedException extends RuntimeException{
+//    public UnauthorizedException(String message) {
+//        super(message);
+//    }
+//}
