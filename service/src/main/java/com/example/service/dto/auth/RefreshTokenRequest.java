b243a461170451104b8a804d680d50aeccd359da
tatdat1412 <134081719+tatdat1412@users.noreply.github.com>
2026-01-14 23:00:37 +0700

    adjust RefreshTokenRequest

--- service/src/main/java/com/example/service/dto/auth/RefreshTokenRequest.java
+++ service/src/main/java/com/example/service/dto/auth/RefreshTokenRequest.java
@@ -1,8 +1,12 @@
 package com.example.service.dto.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RefreshTokenRequest {
     private String refreshToken;
 }
