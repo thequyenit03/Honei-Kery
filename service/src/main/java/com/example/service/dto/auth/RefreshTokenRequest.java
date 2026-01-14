a4c333e0fe6faf9282c6e325b508425827dd8ab4
tatdat1412 <134081719+tatdat1412@users.noreply.github.com>
2026-01-14 23:04:39 +0700

    adjust RefreshTokenRequest v2

--- service/src/main/java/com/example/service/dto/auth/RefreshTokenRequest.java
+++ service/src/main/java/com/example/service/dto/auth/RefreshTokenRequest.java
@@ -1,5 +1,6 @@
 package com.example.service.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RefreshTokenRequest {
+    @JsonProperty("refreshToken")
     private String refreshToken;
 }
